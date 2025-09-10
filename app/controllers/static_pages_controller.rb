486ab9eb302e97c6294b1fff5ca043d16f4e8d35
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-09-10 22:57:45 +0900

    ホーム画面ルーティング設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,9 @@
 class StaticPagesController < ApplicationController
-  def home
-  end
+  def home; end
 
-  def terms
-  end
+  def terms; end
 
-  def privacy
-  end
+  def privacy; end
 
-  def contact
-  end
+  def contact; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
+  root "static_pages#home"
+
   get "static_pages/terms"
   get "static_pages/privacy"
   get "static_pages/contact"
