580de2a3f17db7c24220bd25ca922ec45f680f07
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-09-21 23:49:38 +0900

    カラム追加

--- db/migrate/20250920085802_devise_create_users.rb
+++ db/migrate/20250920085802_devise_create_users.rb
@@ -22,23 +22,28 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.2]
       # t.string   :last_sign_in_ip
 
       ## Confirmable
-      # t.string   :confirmation_token
-      # t.datetime :confirmed_at
-      # t.datetime :confirmation_sent_at
-      # t.string   :unconfirmed_email # Only if using reconfirmable
+      t.string   :confirmation_token
+      t.datetime :confirmed_at
+      t.datetime :confirmation_sent_at
+      t.string   :unconfirmed_email # Only if using reconfirmable
 
       ## Lockable
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      ## 追加カラム（ER図に合わせて追加）
+      t.string  :name
+      t.string  :line_uid
+      t.integer :global_role, default: 0, null: false  # enum: member(0), admin(1)
+      t.datetime :deleted_at
 
       t.timestamps null: false
     end
 
     add_index :users, :email,                unique: true
     add_index :users, :reset_password_token, unique: true
-    # add_index :users, :confirmation_token,   unique: true
+    add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
   end
 end
