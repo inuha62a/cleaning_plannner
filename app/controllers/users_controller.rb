32b244aba851e0ba68c3883310003eab4ee28c32
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-10-01 01:15:12 +0900

    管理者制限の範囲修正、パスワード表示Rails7なのでTurboに変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :require_admin
+  before_action :require_admin, except: [:show]
+
+  def show
+    @user = current_user
+  end
 
   def require_admin
     redirect_to root_path, alert: "アクセス権限がありません" unless current_user.admin?
-  end
-  
-  def show
-    @user = current_user
   end
 end
