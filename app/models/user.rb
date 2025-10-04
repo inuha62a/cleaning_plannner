f24eebf2af424df158d7e15576ebb3c920d3ebd9
Itsuki Miyazaki <miyamac@MacBookAir.kkbnj1.kt.home.ne.jp>
2025-10-05 00:08:12 +0900

    Userテーブル側修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,14 @@ class User < ApplicationRecord
          :confirmable # ← Confirmableを使うので追加
   # 権限管理
   enum system_role: { general: 0, admin: 1 }
+
+  # 関連付け
+  has_many :owned_groups, class_name: 'Group', foreign_key: 'owner_id'
+  has_many :memberships
+  has_many :groups, through: :memberships
+  has_many :locations
+  has_many :tasks
+
   # STEP6で追加予定のソフトデリート対応（今は未記述でOK）
   # acts_as_paranoid if column_names.include?("deleted_at")
 end
